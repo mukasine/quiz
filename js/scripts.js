aacae021dfcfa73f1f07c25f6385d4214ee6002b
nyirahabineza <habcecile@gmail.com>
2018-12-21 16:39:03 +0200

    html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
+var count = 0;
 function test() {
     var qOne = document.test.qOne.value;
-    var qTwo = document.test.qwo.value;
+    var qTwo = document.test.qTwo.value;
     var qThree = document.test.qThree.value;
     var qFour = document.test.qFour.value;
     var qFive = document.test.qFive.value;
@@ -9,8 +10,7 @@ function test() {
     var qEight = document.test.qEight.value;
     var qNine = document.test.qNine.value;
     var qTen = document.test.qTen.value;
-    var count = 0;
-  
+    
     if (qOne == "2") {
       count += 1;
     }
@@ -41,17 +41,24 @@ function test() {
     if (qTen == "4") {
       count += 1;
     }
-    document.getElementById("after_submit").style.visibility = "visible";
+    return count;
+    // document.getElementById("after_submit").style.visibility = "visible";
   
-    document.getElementById("number_count").innerHTML =
-      "Your score is: " + count + "/10";
+    // document.getElementById("number_count").innerHTML =
+    //   "Your score is: " + count + "/10";
   
-    $(document).ready(function() {
-      $("#button").hide();
-      $("#quiz").hide();
-      $("#number_count").show();
-      event.preventDefault();
-    });
+    
   }
-  
+  $(document).ready(function() {
+      $("form").submit(function(){
+        test()
+        $("#button").hide();
+        $("#test").hide();
+        $("#number_count").show(function(){
+            $("#number_count").text("Your score is: " + count + "/10")
+        });
+        event.preventDefault();
+      })
+    
+  });
   
