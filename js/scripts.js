a7ad99d8e014b20e1bc8b21301a57961750e3e30
nyirahabineza <habcecile@gmail.com>
2018-12-21 15:28:45 +0200

    pages modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,50 @@
-function check() {
-    var questionOne = document.quiz.questionOne.value;
-    var questionTwo = document.quiz.questionTwo.value;
-    var questionThree = document.quiz.questionThree.value;
-    var questionFour = document.quiz.questionFour.value;
-    var questionFive = document.quiz.questionFive.value;
-    var questionSix = document.quiz.questionSix.value;
-    var questionSeven = document.quiz.questionSeven.value;
-    var questionEight = document.quiz.questionEight.value;
-    var questionNine = document.quiz.questionNine.value;
-    var questionTen = document.quiz.questionTen.value;
-    var questionEleven = document.quiz.questionEleven.value;
-    var questionTwelve = document.quiz.questionTwelve.value;
-    var questionThirteen = document.quiz.questionThirteen.value;
-    var questionFourteen = document.quiz.questionFourteen.value;
-    var questionFifteen = document.quiz.questionFifteen.value;
+function test() {
+    var qOne = document.test.qOne.value;
+    var qTwo = document.test.qwo.value;
+    var qThree = document.test.qThree.value;
+    var qFour = document.test.qFour.value;
+    var qFive = document.test.qFive.value;
+    var qSix = document.test.qSix.value;
+    var qSeven = document.test.qSeven.value;
+    var qEight = document.test.qEight.value;
+    var qNine = document.test.qNine.value;
+    var qTen = document.test.qTen.value;
     var count = 0;
   
-    if (questionOne == "2") {
-      count += 4;
+    if (qOne == "2") {
+      count += 1;
     }
-    if (questionTwo == "2") {
-      count += 4;
+    if (qTwo == "2") {
+      count += 1;
     }
-    if (questionThree == "3") {
-      count += 4;
+    if (qThree == "3") {
+      count += 1;
     }
-    if (questionFour == "3") {
-      count += 4;
+    if (qFour == "3") {
+      count += 1;
     }
-    if (questionFive == "4") {
-      count += 4;
+    if (qFive == "4") {
+      count += 1;
     }
-    if (questionSix == "2") {
-      count += 4;
+    if (qSix == "2") {
+      count += 1;
     }
-    if (questionSeven == "2") {
-      count += 4;
+    if (qSeven == "2") {
+      count += 1;
     }
-    if (questionEight == "3") {
-      count += 4;
+    if (qEight == "3") {
+      count += 1;
     }
-    if (questionNine == "2") {
-      count += 4;
+    if (qNine == "2") {
+      count += 1;
     }
-    if (questionTen == "4") {
-      count += 4;
+    if (qTen == "4") {
+      count += 1;
     }
-    if (questionEleven == "1") {
-      count += 4;
-    }
-    if (questionTwelve == "3") {
-      count += 4;
-    }
-    if (questionThirteen == "4") {
-      count += 4;
-    }
-    if (questionFourteen == "1") {
-      count += 4;
-    }
-    if (questionFifteen == "4") {
-      count += 4;
-    }
-  
     document.getElementById("after_submit").style.visibility = "visible";
   
     document.getElementById("number_count").innerHTML =
-      "Your score is: " + count + "/60";
+      "Your score is: " + count + "/10";
   
     $(document).ready(function() {
       $("#button").hide();
